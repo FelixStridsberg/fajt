39802c00f19aa219f308c17a364f6c15ed3444f6
Felix Stridsberg <felixstridsberg@gmail.com>
2021-04-27 20:08:42 +0200

    doc: documenting testing macro

--- lexer/tests/lib.rs
+++ lexer/tests/lib.rs
@@ -1,4 +1,24 @@
-/// TODO documentation
+/// Test macro for easily running the lexer.
+///
+/// Assert output:
+/// ```
+/// assert_lexer!(
+///     // String input to the lexer
+///     input: "var i = 0;",
+///
+///     // The output is a tuple of the token and the span.
+///     // Use macros for easily creating tokens.
+///     output: [
+///         (keyword!("var"), (0, 3)),
+///         (identifier!("variable"), (4, 12)),
+///         (punct!("="), (13, 14)),
+///         (literal!(integer, 1), (15, 16)),
+///     ]
+/// )
+/// ```
+///
+/// Assert error:
+/// TODO, syntax not finalized
 #[macro_export]
 macro_rules! assert_lexer {
     (input: $input:expr, output: [$(($token:expr, ($col1:expr, $col2:expr)),)*]) => {
@@ -15,6 +35,8 @@ macro_rules! assert_lexer {
     };
 }
 
+/// Macro for creating identifier, currently there is non in the lexer library, use that one instead
+/// if created, not sure if there will be a need for one except for testing.
 #[macro_export]
 macro_rules! identifier {
     ($name:expr) => {
