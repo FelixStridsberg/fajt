2a1467fb6e1a397af495e0dc9af09a35bf558de7
Felix Stridsberg <felixstridsberg@gmail.com>
2021-05-02 20:59:56 +0200

    fix: use match macro for stmts as well

--- parser/src/statement.rs
+++ parser/src/statement.rs
@@ -3,24 +3,25 @@ use crate::error::Result;
 use crate::Parser;
 use fajt_lexer::keyword;
 use fajt_lexer::punct;
+use fajt_lexer::token_matches;
 
 mod variable;
 
 impl Parser<'_> {
     pub(crate) fn parse_statement(&mut self) -> Result<Stmt> {
-        Ok(match self.reader.current().value {
-            punct!(";") => Stmt::Empty(EmptyStmt::new(self.reader.current().location.clone())),
-            punct!("{") => unimplemented!("BlockStatement"),
-            keyword!("var") => self.parse_variable_statement(VariableKind::Var)?,
-            keyword!("if") => unimplemented!("IfStatement"),
-            keyword!("break") => unimplemented!("BreakStatement"),
-            keyword!("continue") => unimplemented!("ContinueStatement"),
-            keyword!("break") => unimplemented!("BreakStatement"),
-            keyword!("return") => unimplemented!("ReturnStatement"),
-            keyword!("with") => unimplemented!("WithStatement"),
-            keyword!("throw") => unimplemented!("ThrowStatement"),
-            keyword!("try") => unimplemented!("TryStatement"),
-            keyword!("debugger") => unimplemented!("DebuggerStatement"),
+        Ok(match self.reader.current() {
+            token_matches!(punct!(";")) => Stmt::Empty(EmptyStmt::new(self.reader.current().location.clone())),
+            token_matches!(punct!("{")) => unimplemented!("BlockStatement"),
+            token_matches!(keyword!("var")) => self.parse_variable_statement(VariableKind::Var)?,
+            token_matches!(keyword!("if")) => unimplemented!("IfStatement"),
+            token_matches!(keyword!("break")) => unimplemented!("BreakStatement"),
+            token_matches!(keyword!("continue")) => unimplemented!("ContinueStatement"),
+            token_matches!(keyword!("break")) => unimplemented!("BreakStatement"),
+            token_matches!(keyword!("return")) => unimplemented!("ReturnStatement"),
+            token_matches!(keyword!("with")) => unimplemented!("WithStatement"),
+            token_matches!(keyword!("throw")) => unimplemented!("ThrowStatement"),
+            token_matches!(keyword!("try")) => unimplemented!("TryStatement"),
+            token_matches!(keyword!("debugger")) => unimplemented!("DebuggerStatement"),
             // TODO ExpressionStatement
             // TODO LabelledStatement
             _ => unimplemented!("Invalid statement error handling"),
