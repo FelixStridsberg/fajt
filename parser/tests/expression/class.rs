ee3a8dfe9d72b84e2473e79b3fc8da744cc9ec3b
Felix Stridsberg <felix.stridsberg@gmail.com>
2021-06-06 19:46:05 +0200

    chore: add tests for static semantic errors

--- parser/tests/expression/class.rs
+++ parser/tests/expression/class.rs
@@ -1,5 +1,6 @@
 use fajt_lexer::token::Span;
 use fajt_parser::ast::*;
+use fajt_parser::error::ErrorKind::SyntaxError;
 
 #[test]
 fn empty_class() {
@@ -203,5 +204,41 @@ fn class_empty_setter_method() {
                 ],
             }.into()
         ]
+    );
+}
+
+#[ignore] // TODO
+#[test]
+fn fail_setter_method_multiple_parameters() {
+    parser_test!(
+        input: "class { set setter(a, b) {} }",
+        expr_error: SyntaxError(
+            "Setter must have exactly one formal parameter.".to_owned(),
+            Span::new(18, 24)
+        )
+    );
+}
+
+#[ignore] // TODO
+#[test]
+fn fail_setter_method_without_parameter() {
+    parser_test!(
+        input: "class { set setter() {} }",
+        expr_error: SyntaxError(
+            "Setter must have exactly one formal parameter.".to_owned(),
+            Span::new(18, 20)
+        )
+    );
+}
+
+#[ignore] // TODO
+#[test]
+fn fail_getter_method_with_parameter() {
+    parser_test!(
+        input: "class { set setter(a) {} }",
+        expr_error: SyntaxError(
+            "Getter must not have any formal parameters.".to_owned(),
+            Span::new(18, 20)
+        )
     );
 }
