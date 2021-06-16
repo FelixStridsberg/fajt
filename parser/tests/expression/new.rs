9508f4eb30c04f12410d419325220ae8be6cc3b1
Felix Stridsberg <felix.stridsberg@gmail.com>
2021-06-16 19:43:21 +0200

    chore: added test for nested new expression

--- parser/tests/expression/new.rs
+++ parser/tests/expression/new.rs
@@ -13,3 +13,19 @@ fn new() {
         ]
     );
 }
+
+#[test]
+fn new_nested() {
+    parser_test!(
+        input: "new new a",
+        expr_output: [
+            NewExpression {
+                span: Span::new(0, 9),
+                callee: NewExpression {
+                    span: Span::new(4, 9),
+                    callee: IdentifierReference::Ident(Ident::new("a", (8, 9))).into(),
+                }.into(),
+            }.into()
+        ]
+    );
+}
