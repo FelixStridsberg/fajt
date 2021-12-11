6b772788b60eb2c5ed47cba6645e59caa0d7d7b1
Felix Stridsberg <felix.stridsberg@gmail.com>
2021-12-11 19:49:10 +0100

    feat: exit and trace visitor for tests

--- traverser/src/fold.rs
+++ traverser/src/fold.rs
@@ -1,7 +1,7 @@
 macro_rules! generate_fold {
     (
         $(
-            $ident:ident $( <$param:ident> )? (enter: $enter:ident) {
+            $ident:ident $( <$param:ident> )? (enter: $enter:ident, exit: $exit:ident) {
                 $(
                     $field: ident
                 )*
@@ -10,15 +10,14 @@ macro_rules! generate_fold {
     ) => {
         $(
             impl Fold for $ident $( <$param> )? {
-                fn fold(self, _visitor: &mut dyn Visitor) -> Self {
-                    println!("Traverse: {}", stringify!($ident));
-
-                    let node = _visitor.$enter(self);
-
-                    $ident {
-                        $($field: node.$field.fold(_visitor),)*
+                fn fold(self, visitor: &mut dyn Visitor) -> Self {
+                    let node = visitor.$enter(self);
+                    let folded = $ident {
+                        $($field: node.$field.fold(visitor),)*
                         ..node
-                    }
+                    };
+
+                    visitor.$exit(folded)
                 }
             }
         )*
@@ -26,7 +25,7 @@ macro_rules! generate_fold {
 
     (
         $(
-            enum $ident:ident (enter: $enter:ident) {
+            enum $ident:ident (enter: $enter:ident, exit: $exit: ident) {
                 $(
                     $field: ident
                 )*
@@ -36,14 +35,15 @@ macro_rules! generate_fold {
         $(
             impl Fold for $ident {
                 fn fold(self, visitor: &mut dyn Visitor) -> Self {
-                    println!("Traverse: {}", stringify!($ident));
                     let node = visitor.$enter(self);
-                    match node {
+                    let folded = match node {
                         $( $ident::$field(v) => $ident::$field(v.fold(visitor)), )*
 
                         #[allow(unreachable_patterns)]
                         _ => node
-                    }
+                    };
+
+                    visitor.$exit(folded)
                 }
             }
         )*
--- traverser/src/lib.rs
+++ traverser/src/lib.rs
@@ -6,32 +6,59 @@ use fajt_ast::{
     StatementList, Stmt, StmtExpr,
 };
 
-macro_rules! generate_visit_fn {
+macro_rules! generate_visit_trait {
     (
         $( $fn_name:ident: $node:ident $( <$param:ident> )?  )*
     ) => {
-        $(
-            fn $fn_name(&mut self, node: $node$( <$param> )?) -> $node$( <$param> )? {
-                node
-            }
-        )*
+        trait Visitor {
+            $(
+                fn $fn_name(&mut self, node: $node$( <$param> )?) -> $node$( <$param> )? {
+                    node
+                }
+            )*
+        }
+
+        #[cfg(test)]
+        struct TraceVisitor {
+            pub visits: Vec<&'static str>,
+        }
+
+        #[cfg(test)]
+        impl Visitor for TraceVisitor {
+            $(
+                fn $fn_name(&mut self, node: $node$( <$param> )?) -> $node$( <$param> )? {
+                    self.visits.push(stringify!($fn_name));
+                    node
+                }
+            )*
+        }
     }
 }
 
-trait Visitor {
-    generate_visit_fn! {
-        enter_ident: Ident
-        enter_body: Body
-        enter_function_decl: DeclFunction
-        enter_stmt_expr: StmtExpr
-        enter_binary_expr: ExprBinary
-        enter_stmt_list: StatementList<Stmt>
-        enter_binding_element: BindingElement
-        enter_format_parameters: FormalParameters
-        enter_program: Program
-        enter_expr: Expr
-        enter_stmt: Stmt
-    }
+generate_visit_trait! {
+    enter_ident: Ident
+    enter_body: Body
+    enter_function_decl: DeclFunction
+    enter_stmt_expr: StmtExpr
+    enter_binary_expr: ExprBinary
+    enter_stmt_list: StatementList<Stmt>
+    enter_binding_element: BindingElement
+    enter_format_parameters: FormalParameters
+    enter_program: Program
+    enter_expr: Expr
+    enter_stmt: Stmt
+
+    exit_ident: Ident
+    exit_body: Body
+    exit_function_decl: DeclFunction
+    exit_stmt_expr: StmtExpr
+    exit_binary_expr: ExprBinary
+    exit_stmt_list: StatementList<Stmt>
+    exit_binding_element: BindingElement
+    exit_format_parameters: FormalParameters
+    exit_program: Program
+    exit_expr: Expr
+    exit_stmt: Stmt
 }
 
 trait Fold {
@@ -51,73 +78,63 @@ impl<F: Fold> Fold for Box<F> {
 }
 
 generate_fold! {
-    enum Program(enter: enter_program) {
+    enum Program(enter: enter_program, exit: exit_program) {
         Script
         Module
     }
 
-    enum Expr(enter: enter_expr) {
+    enum Expr(enter: enter_expr, exit: exit_expr) {
         Binary
         IdentRef
     }
 
-    enum Stmt(enter: enter_stmt) {
+    enum Stmt(enter: enter_stmt, exit: exit_stmt) {
         FunctionDecl
         Expr
     }
 }
 
 generate_fold! {
-    Body(enter: enter_body) {
+    Body(enter: enter_body, exit: exit_body) {
         statements
     }
 
-    DeclFunction(enter: enter_function_decl) {
+    DeclFunction(enter: enter_function_decl, exit: exit_function_decl) {
         identifier
         parameters
         body
     }
 
-    StmtExpr(enter: enter_stmt_expr) {
+    StmtExpr(enter: enter_stmt_expr, exit: exit_stmt_expr) {
         expr
     }
 
-    ExprBinary(enter: enter_binary_expr) {
+    ExprBinary(enter: enter_binary_expr, exit: exit_binary_expr) {
         left
         right
     }
 
-    StatementList<Stmt>(enter: enter_stmt_list) {
+    StatementList<Stmt>(enter: enter_stmt_list, exit: exit_stmt_list) {
         body
     }
 
-    BindingElement(enter: enter_binding_element) { }
+    BindingElement(enter: enter_binding_element, exit: exit_binding_element) { }
 
-    FormalParameters(enter: enter_format_parameters) {
+    FormalParameters(enter: enter_format_parameters, exit: exit_format_parameters) {
         bindings
     }
 
-    Ident (enter: enter_ident) {}
+    Ident (enter: enter_ident, exit: exit_ident) {}
 }
 
 #[cfg(test)]
 mod tests {
     use super::Fold;
-    use crate::Visitor;
+    use crate::{ TraceVisitor, Visitor };
     use fajt_ast::{
         BinaryOperator, BindingElement, Body, DeclFunction, ExprBinary, FormalParameters, Ident,
         Program, Span, StatementList, Stmt, StmtExpr,
     };
-
-    struct IdentModifier {}
-
-    impl Visitor for IdentModifier {
-        fn enter_ident(&mut self, mut ident: Ident) -> Ident {
-            println!("VISITED: {:?}", ident);
-            ident.name = "Other name".to_string();
-            ident
-        }
-    }
 
     #[test]
     fn it_works() {
@@ -155,10 +172,10 @@ mod tests {
             .into()],
         });
 
-        let mut visitor = IdentModifier {};
-        let ast2 = ast.fold(&mut visitor);
+        let mut visitor = TraceVisitor { visits: Vec::new() };
+        ast.fold(&mut visitor);
 
-        println!("AST2 {:?}", ast2);
+        println!("AST2 {:?}", visitor.visits);
 
         assert_eq!(1, 2);
     }
