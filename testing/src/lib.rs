5ea39b5e3405a07ce75478efc49cfde4071831ca
Felix Stridsberg <felix.stridsberg@gmail.com>
2022-01-12 21:42:21 +0100

    fix(testing): inconsistent test file output

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -5,8 +5,13 @@ use std::path::Path;
 pub mod markdown;
 
 pub fn write_string(path: &Path, string: &str) {
-    let mut file = OpenOptions::new().write(true).open(path).unwrap();
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(path)
+        .unwrap();
     file.write_all(string.as_bytes()).unwrap();
+    file.write_all("\n".as_bytes()).unwrap();
 }
 
 pub fn read_string(path: &Path) -> String {
