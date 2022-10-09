82a36721911bd708d539e91fcf32377be3cbe74d
Felix Stridsberg <felix.stridsberg@gmail.com>
2022-10-09 17:27:20 +0200

    chore: temporary error printing for easier development

--- parser/src/cover.rs
+++ parser/src/cover.rs
@@ -16,9 +16,16 @@ where
         let start_token = self.current()?.clone();
         match self.parse_parenthesized_expr() {
             Ok(expr) if !self.current_matches(&punct!("=>")) => Ok(expr),
-            _ => {
+            result => {
                 self.reader.rewind_to(&start_token)?;
-                self.parse_arrow_function_expr()
+
+                let arrow_function = self.parse_arrow_function_expr();
+                if result.is_err() && arrow_function.is_err() {
+                    // TODO make real error handling
+                    println!("Error during cover case:\n{:?}", result.unwrap_err());
+                }
+
+                arrow_function
             }
         }
     }
@@ -28,9 +35,15 @@ where
         let start_token = self.current()?.clone();
         match self.parse_covered_call_expression() {
             Ok(expr) if !self.current_matches(&punct!("=>")) => Ok(expr),
-            _ => {
+            result => {
                 self.reader.rewind_to(&start_token)?;
-                self.parse_async_arrow_function_expr()
+                let arrow_function = self.parse_async_arrow_function_expr();
+                if result.is_err() && arrow_function.is_err() {
+                    // TODO make real error handling
+                    println!("Error during cover case:\n{:?}", result.unwrap_err());
+                }
+
+                arrow_function
             }
         }
     }
