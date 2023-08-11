809b3d8fb477e6e7725a42234b593965b9cb9ed3
Felix Stridsberg <felix.stridsberg@gmail.com>
2023-08-11 22:22:47 +0200

    chore: add failing comment tests

--- lexer/tests/comments.rs
+++ lexer/tests/comments.rs
@@ -18,7 +18,31 @@ fn no_new_line_in_multi_line_comment() {
 }
 
 #[test]
+fn no_space_between_comments() {
+    let tokens = lex("/* Hello, I am comment. *//**/b");
+    assert!(!tokens[1].first_on_line);
+}
+
+#[test]
 fn new_line_in_multi_line_comment() {
     let tokens = lex("a/* Hello,\nI am comment. */b");
     assert!(tokens[1].first_on_line);
+}
+
+#[test]
+fn single_line_html_open_comment() {
+    let tokens = lex("<!-- Hello, I am comment.\na");
+    assert_eq!(tokens.len(), 1);
+}
+
+#[test]
+fn single_line_html_close_comment() {
+    let tokens = lex("--> Hello, I am comment.\na");
+    assert_eq!(tokens.len(), 1);
+}
+
+#[test]
+fn single_line_html_close_comment_comment_before() {
+    let tokens = lex("/* hello */ /* hello */ --> Hello, I am comment.\na");
+    assert_eq!(tokens.len(), 1);
 }
